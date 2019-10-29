6c4ecd5f6ba66d65d933f04a96f2074d456664a1
Serhii Minin <minin.serhii@gmail.com>
2019-10-29 15:42:30 +0200

    feat: use editor config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+const path = require('path');
+
+// console.log(path);
 const func = (name: string): string => `Hello, ${name}`;
+
+function b() {
+    console.log(module);
+    console.log(this);
+}
+
+b();
