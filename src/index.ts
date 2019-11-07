51c34cf3b3696e8182bc1181b439502e57235154
Serhii Minin <minin.serhii@gmail.com>
2019-11-07 14:17:11 +0200

    feat: use nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-const path = require('path');
-const util = require('util');
-const fs = require('fs');
+const path = require("path");
+const util = require("util");
+const fs = require("fs");
 
 const readdir = util.promisify(fs.readdir);
-
-const func = (name: string): string => `Hello, ${name}`;
-
 readdir(__dirname).then(console.log);
+
+console.log({ __dirname, __filename });
