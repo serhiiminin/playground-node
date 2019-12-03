dbe39b4fc19cc5c7aff479097026db7f2646ec12
Serhii Minin <minin.serhii@gmail.com>
2019-12-03 21:55:13 +0200

    use basic modules

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
-const path = require('path');
-const util = require('util');
-const fs = require('fs');
-
-const readdir = util.promisify(fs.readdir);
-readdir(__dirname).then(console.log);
-
-console.log({ __dirname, __filename });
+require('./modules/http');
+require('./modules/fs');
---
+++ src/modules/fs.ts
@@ -0,0 +1,8 @@
+const path = require('path');
+const util = require('util');
+const fs = require('fs');
+
+const readdir = util.promisify(fs.readdir);
+readdir(__dirname).then(console.log);
+
+console.log({ __dirname, __filename });
